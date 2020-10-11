8856f4c993165f6c1f63aaaf2917b9df76589bdf
Екатерина Москивец <4katya88@mail.ru>
2020-10-11 11:49:58 +0300

    Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-const similarList = userDialog.querySelector('.setup-similar-list');
+const setup = document.querySelector('.setup');
+const setupOpen = document.querySelector('.setup-open');
+const setupClose = document.querySelector('.setup-close');
+
+const similarList = setup.querySelector('.setup-similar-list');
 const similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 const names = [
@@ -36,12 +38,20 @@ const coatColors = [
   'rgb(0, 0, 0)'
 ];
 
-const eyesColor = [
+const eyesColors = [
   'black',
   'red',
   'blue',
   'yellow',
   'green'
+];
+
+const fireballColors = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
 ];
 
 const getRndInteger = function (arr) {
@@ -54,22 +64,22 @@ const wizards = [
   {
     name: `${getRndInteger(names)} ${getRndInteger(secondNames)}`,
     coatColor: getRndInteger(coatColors),
-    eyesColor: getRndInteger(eyesColor)
+    eyesColors: getRndInteger(eyesColors)
   },
   {
     name: `${getRndInteger(names)} ${getRndInteger(secondNames)}`,
     coatColor: getRndInteger(coatColors),
-    eyesColor: getRndInteger(eyesColor)
+    eyesColors: getRndInteger(eyesColors)
   },
   {
     name: `${getRndInteger(names)} ${getRndInteger(secondNames)}`,
     coatColor: getRndInteger(coatColors),
-    eyesColor: getRndInteger(eyesColor)
+    eyesColors: getRndInteger(eyesColors)
   },
   {
     name: `${getRndInteger(names)} ${getRndInteger(secondNames)}`,
     coatColor: getRndInteger(coatColors),
-    eyesColor: getRndInteger(eyesColor)
+    eyesColors: getRndInteger(eyesColors)
   }
 ];
 
@@ -78,7 +88,7 @@ const renderWizard = function (wizard) {
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColors;
 
   return wizardElement;
 };
@@ -95,4 +105,69 @@ const fillWizard = function () {
 
 fillWizard();
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
+
+// Change wizart roperties module4-task1
+
+const setupPlayer = document.querySelector('.setup-player');
+const wizartCoat = setupPlayer.querySelector('.setup-wizard .wizard-coat');
+const wizardEyes = setupPlayer.querySelector('.setup-wizard .wizard-eyes');
+const wizartFireball = document.querySelector('.setup-fireball-wrap');
+
+const inputCoat = setupPlayer.querySelector('input[name="coat-color"]');
+const inputEyes = setupPlayer.querySelector('input[name="eyes-color"]');
+const inputFireball = setupPlayer.querySelector('input[name="fireball-color"]');
+
+const changeWizartProperties = function (elem, input, arr) {
+
+  if (elem === wizartFireball) {
+    elem.style.backgroundColor = getRndInteger(arr);
+  }
+
+  elem.style.fill = getRndInteger(arr);
+  input.value = elem.style.fill;
+};
+
+setupPlayer.addEventListener('click', function (evt) {
+  if (evt.target === wizartCoat) {
+    changeWizartProperties(wizartCoat, inputCoat, coatColors);
+  } else if (evt.target === wizardEyes) {
+    changeWizartProperties(wizardEyes, inputEyes, eyesColors);
+  } else if (evt.target.closest('.setup-fireball-wrap')) {
+    changeWizartProperties(wizartFireball, inputFireball, fireballColors);
+  }
+});
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
